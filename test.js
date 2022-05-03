012a5daca1da868cf13ec9964a48aba594f67e89
rennaarenata <59098319+rennaarenata@users.noreply.github.com>
2022-05-03 16:50:33 +0200

    -

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-const execSync = require('child_process').execSync;
-
-
-const output = execSync('ls', { encoding: 'utf-8' });
-console.log('Output was:\n', output);
-                                                                                           //no harm intende
+//JS file used for ctfs 
+//I don't want to harm anyone or anything
+// Have a good day :)
